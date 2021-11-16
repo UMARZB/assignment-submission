9926cfab9fccb3c33fadca71cbe33eed427f1f3f
UMARZB <94454420+UMARZB@users.noreply.github.com>
2021-11-16 18:57:59 +0530

    day 1 assignment submitted

---
+++ day-1.py
@@ -0,0 +1,21 @@
+1)PRINT MY NAME:
+
+print('HAI I AM MOHAMED')
+
+OUTPUT:
+
+HAI I AM MOHAMED
+
+
+
+2)ADD TWO NUMBERS:
+
+num1 = 1.5
+num2 = 6.3
+sum = num1 + num2
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+
+OUTPUT:
+
+The sum of 1.5 and 6.3 is 7.8
